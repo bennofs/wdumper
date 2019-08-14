0c628ef95183842cc3209e323dece8b1cfc31ebf
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2019-08-14 23:21:12 +0200

    frontend sql: switch to mysqlclient driver

--- frontend/config.py
+++ frontend/config.py
@@ -7,7 +7,7 @@ DB_NAME = os.getenv("DB_NAME", "wdumper")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DB_URI = "mysql+mysqlconnector://{}@{}/{}".format(DB_USER + (":" + DB_PASSWORD if DB_PASSWORD is not None else ""), DB_HOST, DB_NAME)
+DB_URI = "mysql+mysqldb://{}@{}/{}".format(DB_USER + (":" + DB_PASSWORD if DB_PASSWORD is not None else ""), DB_HOST, DB_NAME)
 DUMPS_PATH = os.getenv("DUMPS_PATH", os.path.join(basedir, "dumpfiles/generated"))
 ZENODO_SANDBOX_TOKEN = os.getenv("ZENODO_SANDBOX_TOKEN", "")
 ZENODO_TOKEN = os.getenv("ZENODO_TOKEN", "")
