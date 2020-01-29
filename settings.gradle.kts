315c337da67deda0a3bac989d618d8543a4644a2
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2020-01-29 16:18:28 +0100

    refactor: remove unnecessary files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,3 +9,18 @@
 
 rootProject.name = "wdumper"
 include("jooq-liquibase")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "cz.habarta.typescript-generator") {
+                useModule("cz.habarta.typescript-generator:typescript-generator-gradle-plugin:${requested.version ?: "+"}")
+            }
+        }
+    }
+}
--- src/main/sql/V2__make_stats_bigint.sql
+++
@@ -1,4 +0,0 @@
--- the _count fields in the dump table should be BIGINT
-ALTER TABLE `dump` MODIFY COLUMN `entity_count` BIGINT NOT NULL DEFAULT 0;
-ALTER TABLE `dump` MODIFY COLUMN `statement_count` BIGINT NOT NULL;
-ALTER TABLE `dump` MODIFY COLUMN `triple_count` BIGINT NOT NULL;
