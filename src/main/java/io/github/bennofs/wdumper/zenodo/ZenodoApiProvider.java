bd86f8c97f9d82ef0517eb17ebdb67295ba56267
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2020-04-21 15:35:26 +0200

    docs(ZenodoApiProvider): document interface

--- src/main/java/io/github/bennofs/wdumper/zenodo/ZenodoApiProvider.java
+++ src/main/java/io/github/bennofs/wdumper/zenodo/ZenodoApiProvider.java
@@ -5,7 +5,17 @@ import io.github.bennofs.wdumper.zenodo.ZenodoApi;
 
 import javax.annotation.Nullable;
 
+/**
+ * An interface to retrieve the Zenodo API instance for a given target (main zenodo or sandbox).
+ */
 public interface ZenodoApiProvider {
+    /**
+     * Get the ZenodoApi instance for the given target
+     *
+     * @param target API target (main or sandbox)
+     * @return ZenodoApi instance, or null if no instance is available for the given target
+     *         (for example, this can happen if no API key was configured for that target)
+     */
     @Nullable
     ZenodoApi getZenodoApiFor(ZenodoTarget target);
 }
