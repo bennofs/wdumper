cc600e27263bad0cfe453e2184c68b2b0772dfc8
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2022-04-08 13:06:34 +0200

    deploy/clear-failed-run.sh: allow clearing more than 4 dumps at once

--- deploy/clear-failed-run.sh
+++ deploy/clear-failed-run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ ! $# -eq 1 ]; then
-  echo "usage: clear-failed-run.sh RUNID"
+if [ ! \( $# -eq 1 -o $# -eq 2 \) ]; then
+  echo "usage: clear-failed-run.sh RUNID [LIMIT]"
   exit 22
 fi
 
 runid=$1
-mysql --defaults-file=~/tools.cnf s54044__wdumper --batch --execute "UPDATE dump SET run_id = NULL WHERE run_id = $runid LIMIT 4"
+mysql --defaults-file=~/tools.cnf s54044__wdumper --execute "UPDATE dump SET run_id = NULL WHERE run_id = $runid LIMIT ${2:-4}"
 
